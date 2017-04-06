666ed7cc0fabfd3f918a3b0aba59a2afeeb6f6ce
Mordenkainen <shardrill_t@yahoo.com>
2017-04-06 15:37:36 -0400

    How did I miss this file?

---
+++ src/main/java/com/mordenkainen/equivalentenergistics/blocks/crafter/tiles/TileEMCCrafter.java
@@ -0,0 +1,11 @@
+package com.mordenkainen.equivalentenergistics.blocks.crafter.tiles;
+
+import com.mordenkainen.equivalentenergistics.blocks.crafter.BlockEMCCrafter;
+
+public class TileEMCCrafter extends TileEMCCrafterBase {
+
+    public TileEMCCrafter() {
+        super(1, BlockEMCCrafter.craftingTime, 0);
+    }
+
+}
