6787a166c649a519315f0c5c4ce006e5e36bd665
Mordenkainen <shardrill_t@yahoo.com>
2018-10-14 11:25:44 -0400

    Version number

--- src/main/java/com/mordenkainen/equivalentenergistics/core/Reference.java
+++ src/main/java/com/mordenkainen/equivalentenergistics/core/Reference.java
@@ -3,7 +3,7 @@ package com.mordenkainen.equivalentenergistics.core;
 public final class Reference {
 
     public static final String MOD_ID = "equivalentenergistics";
-    public static final String MOD_VERSION = "0.8.4";
+    public static final String MOD_VERSION = "0.8.3";
     public static final String MOD_NAME = "Equivalent Energistics";
     public static final String MOD_DEPENDENCIES = "required-after:appliedenergistics2;after:EE3;after:ProjectE";
     public static final String PROXY_LOC = "com.mordenkainen.equivalentenergistics.core.proxy.";
