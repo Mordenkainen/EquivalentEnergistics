f272aed3820442b93edf5647f1ced851b99e1a84
Mike Caron <caron.mike@outlook.com>
2019-05-15 00:19:57 -0400

    bump the minimum ProjectE version

--- src/main/java/com/mordenkainen/equivalentenergistics/core/Reference.java
+++ src/main/java/com/mordenkainen/equivalentenergistics/core/Reference.java
@@ -5,7 +5,7 @@ public final class Reference {
     public static final String MOD_ID = "equivalentenergistics";
     public static final String MOD_VERSION = "0.8.3";
     public static final String MOD_NAME = "Equivalent Energistics";
-    public static final String MOD_DEPENDENCIES = "required-after:appliedenergistics2;required-after:projecte@[1.12-PE1.3.1B,)";
+    public static final String MOD_DEPENDENCIES = "required-after:appliedenergistics2;required-after:projecte@[1.12.2-PE1.4.1,)";
     public static final String PROXY_LOC = "com.mordenkainen.equivalentenergistics.core.proxy.";
 
     private Reference() {}
